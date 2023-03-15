8b17ae652dda2733be43067e3c3ecaa9ef6836b1
snm410 <snm410@alumni.ku.dk>
2023-03-15 17:02:23 +0100

    1st q3 solution

--- inauguralproject/Household.py
+++ inauguralproject/Household.py
@@ -122,36 +122,22 @@ class HouseholdSpecializationModelClass:
         sol = self.sol
         opt = SimpleNamespace()
         
-        # a. all possible choices
-        x = np.linspace(0,24,49)
-        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
-    
-        LM = LM.ravel() # vector
-        HM = HM.ravel()
-        LF = LF.ravel()
-        HF = HF.ravel()
+        # constrains
+        def u(x):
+            value = -self.calc_utility(x[0],x[1],x[2],x[3])
+            if x[0]+[1]>24:
+                value = -np.inf
+            elif x[2]+x[3]>24:
+                value = -np.inf
+            return value
 
-        # b. calculate utility
-        u = self.calc_utility(LM,HM,LF,HF)
-    
-        # c. set to minus infinity if constraint is broken
-        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
-        u[I] = -np.inf
-    
-        # d. find maximizing argument
-        j = np.argmax(u)
-        
-        opt.LM = LM[j]
-        opt.HM = HM[j]
-        opt.LF = LF[j]
-        opt.HF = HF[j]
+        # optimize
+        res = optimize.minimize(u,[1,1,1,1],method='Nelder-Mead')
 
-        # e. print
-        if do_print:
-            for k,v in opt.__dict__.items():
-                print(f'{k} = {v:6.4f}')
+        # save allocation
+        dict = {'LM' : res.x[0], 'HM' : res.x[1], 'LF' : res.x[2], 'HF' : res.x[3], 'u' : res.fun}
 
-        return opt
+        return dict
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
